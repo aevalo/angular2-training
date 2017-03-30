553614f389e1f52e3967e3b1ed3699229dce69a3
Arttu Valo <arttu.valo@gmail.com>
2017-03-30 12:41:36 +0300

    Foo

--- src/app/search/search.service.ts
+++ src/app/search/search.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
-import {Observable} from 'rxjs/Rx';
 import 'rxjs/Rx';
+import {Observable} from 'rxjs/Rx';
 
 @Injectable()
 export class SearchService {
