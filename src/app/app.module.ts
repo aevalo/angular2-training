226e0d5a79d6c4e97c53d176c0a28737bb770313
Arttu Valo <arttu.valo@gmail.com>
2017-03-30 10:15:38 +0300

    Added new search module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {RouterModule} from '@angular/router';
 
+import {SearchModule} from './search/search.module';
+
 import {AppComponent} from './app.component';
 import {TodosComponent} from './todos/todos.component';
 import {TodoComponent} from './todo/todo.component';
 import {EditTodoComponent} from './edit-todo/edit-todo.component';
 import {TodoService} from './todo.service';
+
 import {routes} from './routes';
 
 @NgModule({
   declarations: [AppComponent, TodosComponent, TodoComponent, EditTodoComponent],
-  imports: [BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(routes), SearchModule],
   providers: [TodoService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
